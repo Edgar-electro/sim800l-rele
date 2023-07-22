79b0d609493f5d6f1e40ac1f47b7bc59f99aa5b9
MUSIC-METRIX <108461028+Edgar-electro@users.noreply.github.com>
2023-07-22 17:46:03 +0300

    Update im_havaqac_sim800l_rele.ino

--- im_havaqac_sim800l_rele.ino
+++ im_havaqac_sim800l_rele.ino
@@ -1,70 +1,79 @@
-/*************************************************************************************
- *  Created By: Tauseef Ahmad
- *  Created On: 15 April, 2023
- *  
- *  Tutorial: https://youtu.be/lYN8hqNAKbU
- *  My Channel: https://www.youtube.com/channel/UCOXYfOHgu-C-UfGyDcu5sYw/
- ***********************************************************************************/
- 
+#include <SoftwareSerial.h>
+#include <AltSoftSerial.h>
+#include <TinyGPS++.h>
 #include <SIM800L.h>
 
-SIM800L sim800l(2, 3); //Rx, Tx
+TinyGPSPlus gps;
+SoftwareSerial gpsSerial(8, 9); // Rx, Tx for GPS module
+SIM800L sim800l(2, 3); // Rx, Tx for GSM module
 
 #define LED_PIN 7
 const String PHONE = "+33769888360";
 boolean STATE_LEDPIN = 0;
 
-
-
 void handleSMS(String number, String message) {
   Serial.println("number: " + number + "\nMessage: " + message);
-  if(number == PHONE){
-    if(message == "on") {
+  if (number == PHONE) {
+    if (message == "on") {
       digitalWrite(LED_PIN, LOW);
       sim800l.sendSMS(number, "LED is ON");
       STATE_LEDPIN = 1;
-    } 
-    else if(message == "off") {
+    } else if (message == "off") {
       digitalWrite(LED_PIN, HIGH);
       sim800l.sendSMS(number, "LED is OFF");
-     STATE_LEDPIN = 0;
+      STATE_LEDPIN = 0;
+    } else if (message == "status") {
+      String text = (STATE_LEDPIN) ? "ON" : "OFF";
+      sim800l.sendSMS(number, "STATUS IS " + text);
+    } else if (message == "Get Location") {
+      String location = getLocation();
+      String googleMapsLink = getGoogleMapsLink(location);
+      sim800l.sendSMS(number, googleMapsLink);
     }
   }
-  
-  if(number == PHONE)  {
-  if(message == "status"){
-    String text = (STATE_LEDPIN)? "ON" : "OFF";
-    sim800l.sendSMS(number,"STATUS IS "+text);
-  }
-    
 }
-}
+
 void handleCall(String number) {
   Serial.println("New call from " + number);
-  if(number == "0769888360"){
+  if (number == PHONE) {
     boolean flag = digitalRead(LED_PIN);
     digitalWrite(LED_PIN, !flag);
-  delay (500);
-  String text = (flag)? "ON" : "OFF";
-    sim800l.sendSMS(number,"LED STAT "+text);
-  
-  }
-  else {
+    delay(500);
+    String text = (flag) ? "ON" : "OFF";
+    sim800l.sendSMS(number, "LED STAT " + text);
+  } else {
     Serial.println("Unkown phone number.");
   }
 }
 
 void setup() {
-  Serial.begin(9600);
-  
+  Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
+  gpsSerial.begin(9600);
   sim800l.begin(9600);
-  
   sim800l.setSMSCallback(handleSMS);
   sim800l.setCallCallback(handleCall);
 }
 
 void loop() {
   sim800l.listen();
+}
+
+String getLocation() {
+  while (gpsSerial.available() > 0) {
+    if (gps.encode(gpsSerial.read())) {
+      if (gps.location.isValid()) {
+        float latitude = gps.location.lat();
+        float longitude = gps.location.lng();
+        String location = String(latitude, 6) + "," + String(longitude, 6);
+        return location;
+      }
+    }
+  }
+  return "Location not available";
+}
+
+String getGoogleMapsLink(String location) {
+  return "https://www.google.com/maps?q=" + location;
 }
